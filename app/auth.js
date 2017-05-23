084faff35fec9f8cabbd1c84ccc1cf67509b37b0
Spencer Carli <spencercarli@gmail.com>
2017-05-23 11:03:55 -0500

    Basic auth logic

--- app/auth.js
+++ app/auth.js
@@ -1,7 +1,21 @@
-export const onSignIn = () => {
-  console.log("handle sign in");
-};
+import { AsyncStorage } from "react-native";
 
-export const onSignOut = () => {
-  console.log("handle sign out");
+export const USER_KEY = "auth-demo-key";
+
+export const onSignIn = () => AsyncStorage.setItem(USER_KEY, "true");
+
+export const onSignOut = () => AsyncStorage.removeItem(USER_KEY);
+
+export const isSignedIn = () => {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem(USER_KEY)
+      .then(res => {
+        if (res !== null) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      })
+      .catch(err => reject(err));
+  });
 };
--- app/index.js
+++ app/index.js
@@ -1,8 +1,34 @@
 import React from "react";
 import { SignedOut, SignedIn } from "./router";
+import { isSignedIn } from "./auth";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false,
+      checkedSignIn: false
+    };
+  }
+  componentWillMount() {
+    isSignedIn()
+      .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+      .catch(err => alert("An error occurred"));
+  }
+
   render() {
-    return <SignedIn />;
+    const { checkedSignIn, signedIn } = this.state;
+
+    // If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
+    if (!checkedSignIn) {
+      return null;
+    }
+
+    if (signedIn) {
+      return <SignedIn />;
+    } else {
+      return <SignedOut />;
+    }
   }
 }
