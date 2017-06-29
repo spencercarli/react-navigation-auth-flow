451b620c5fea763f48a6925ddf4d394661297fff
Spencer Carli <spencercarli@gmail.com>
2017-06-29 15:59:45 -0500

    Don't hide nav and tab bar behind statusbar on android

--- app/router.js
+++ app/router.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Platform, StatusBar } from "react-native";
 import { StackNavigator, TabNavigator } from "react-navigation";
 import { FontAwesome } from "react-native-vector-icons";
 
@@ -7,41 +8,54 @@ import SignIn from "./screens/SignIn";
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 
+const headerStyle = {
+  marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+};
+
 export const SignedOut = StackNavigator({
   SignUp: {
     screen: SignUp,
     navigationOptions: {
-      title: "Sign Up"
+      title: "Sign Up",
+      headerStyle
     }
   },
   SignIn: {
     screen: SignIn,
     navigationOptions: {
-      title: "Sign In"
+      title: "Sign In",
+      headerStyle
     }
   }
 });
 
-export const SignedIn = TabNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      tabBarLabel: "Home",
-      tabBarIcon: ({ tintColor }) => (
-        <FontAwesome name="home" size={30} color={tintColor} />
-      )
+export const SignedIn = TabNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: ({ tintColor }) =>
+          <FontAwesome name="home" size={30} color={tintColor} />
+      }
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarLabel: "Profile",
+        tabBarIcon: ({ tintColor }) =>
+          <FontAwesome name="user" size={30} color={tintColor} />
+      }
     }
   },
-  Profile: {
-    screen: Profile,
-    navigationOptions: {
-      tabBarLabel: "Profile",
-      tabBarIcon: ({ tintColor }) => (
-        <FontAwesome name="user" size={30} color={tintColor} />
-      )
+  {
+    tabBarOptions: {
+      style: {
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+      }
     }
   }
-});
+);
 
 export const createRootNavigator = (signedIn = false) => {
   return StackNavigator(
